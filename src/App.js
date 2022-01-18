fe01601e2a994fa9bcd8d363218e6b013df90ef7
YWTechIT <abcd8637@naver.com>
2022-01-18 17:43:00 +0900

    Fix App.js contennts

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    응애
+                    Learn React
                 </a>
             </header>
         </div>
